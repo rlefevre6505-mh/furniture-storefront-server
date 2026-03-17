eed712427ec62fa891609f7f42372d4aeef7d375
Richard Lefevre <rlefevre.6505@gmail.com>
2026-03-17 14:29:44 +0000

    linked to github repo

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express();
+
+// app.listen(3000, function () {
+//   console.log("Server is listening on port 3000");
+// });
+
+// app.get("/messages", function (request, response) {
+//   response.json({ message: "Hello, World!" });
+// });
+
+app.get("/create-checkout-session", async function () {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: "custom",
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "T-shirt",
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    // return_url: "https://example.com/return?session_id={CHECKOUT_SESSION_ID}",
+    return_url: "localhost:5173",
+  });
+});
