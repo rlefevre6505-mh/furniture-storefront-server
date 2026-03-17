3a5f771ca48f55d510dcdb88eabce0eac9c6b61f
Richard Lefevre <rlefevre.6505@gmail.com>
2026-03-17 14:33:57 +0000

    set up repo

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 // app.listen(3000, function () {
 //   console.log("Server is listening on port 3000");
