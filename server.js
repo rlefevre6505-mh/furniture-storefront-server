bcacf56d26b2813767ae566587c6837c6befa2cc
Richard Lefevre <rlefevre.6505@gmail.com>
2026-03-24 12:50:47 +0000

    updated sever routes

--- server.js
+++ server.js
@@ -6,32 +6,51 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const stripe = new Stripe(process.env.STRIPE_SECRET);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-const PORT = 8080;
-
-app.listen(PORT, () => {
-  console.info(`Server is running in port ${PORT}`);
-});
-
-app.get("/", (request, response) =>
-  response.json({ message: "Welcome to my Stripe Sandbox server!" }),
-);
-
-app.post("/api/create-payment-intent", async (req, res) => {
+app.post("/api/create-checkout-session", async (req, res) => {
   try {
-    const { amount, currency } = req.body;
-
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency,
-      automatic_payment_methods: {
-        enabled: true,
-      },
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items,
+      success_url: "https://https://furniture-storefront.onrender.com/success",
+      cancel_url: "https://https://furniture-storefront.onrender.com/cancel",
     });
 
-    res.json({ clientSecret: paymentIntent.client_secret });
+    res.json({ url: session.url });
   } catch (error) {
+    console.error("Stripe error:", error);
     res.status(500).json({ error: error.message });
   }
 });
+
+// const stripe = new Stripe(process.env.STRIPE_SECRET);
+
+// const PORT = 8080;
+
+// app.listen(PORT, () => {
+//   console.info(`Server is running in port ${PORT}`);
+// });
+
+// app.get("/", (request, response) =>
+//   response.json({ message: "Welcome to my Stripe Sandbox server!" }),
+// );
+
+// app.post("/api/create-payment-intent", async (req, res) => {
+//   try {
+//     const { amount, currency } = req.body;
+
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount,
+//       currency,
+//       automatic_payment_methods: {
+//         enabled: true,
+//       },
+//     });
+
+//     res.json({ clientSecret: paymentIntent.client_secret });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
